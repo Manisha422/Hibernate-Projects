6a37bacc87e8529c201608c798108388eae9d175
manisha <manisharaj422@gmail.com>
2022-10-11 14:05:42 +0530

    commiting file

---
+++ hibernate_manytoone_uni/src/main/java/com/av/hibernatemanytoone/DeleteMobileandSim.java
@@ -0,0 +1,26 @@
+package com.av.hibernatemanytoone;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class DeleteMobileandSim {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("manisha");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction et = em.getTransaction();
+		
+		Sim s = em.find(Sim.class, 4);
+		
+		et.begin();
+		em.remove(s);
+		et.commit();
+		System.out.println("deleted");
+		
+
+	}
+
+}
